6cad7700ab3507acad06088711ad23547294e182
mattia.borrini <mattia.borrini@adesso.ch>
2024-01-25 14:09:22 +0100

    ci(jenkins): docker
    add ApplicationStartListener for print variables at the beginning

---
+++ adapters/src/main/java/dev/skynet/alpha/adapters/listener/ApplicationStartListener.java
@@ -0,0 +1,29 @@
+package dev.skynet.alpha.adapters.listener;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ApplicationStartListener implements ApplicationListener<ApplicationStartedEvent> {
+
+  @Value("${spring.datasource.url}")
+  private String dataSourceUrl;
+
+
+  @Override
+  public void onApplicationEvent(ApplicationStartedEvent ignored) {
+
+
+    log.info("Environment variables for latest version:");
+    log.info("Database Client created with: [baseUrl: {}]",
+             dataSourceUrl
+    );
+
+  }
+}
